f4c1ca2d881464a93f26cab0facecfac0378f1e6
FelixG105 <felix.gonzalez105@gmail.com>
2025-07-22 11:26:42 -0400

    README properly edited

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-# React + Vite
+# What To Wear (WTWR)
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This project provides users with clothing options based on the current weather conditions for their area. Clothing options can be added or removed by the user after registering and signing in. 
 
-Currently, two official plugins are available:
+The project's front end uses React, React Router, Fetch API, LocalStorage, and more.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+The project's backend uses Node, Express, MongoDB, Mongoose, and more.
+
+JWT, Auth Middleware, and Password hashing were used for authentication and security.
+
+Additional techniques used during development include RESTful API Design, Error Handling, Frontend Auth Handling, and more.
+
 
 
 A link to the project can be found here: https://github.com/FelixG105/se_project_react
--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -16,43 +16,29 @@ function ClothesSection({
     <div className="clothes-section">
       <div className="clothes-section__options">
         <p className="clothes-section__title">Your Items</p>
-        {clothingItems
-          .filter((item) => item.owner === currentUser._id)
-          .map((item) => {
-            return (
-              <ItemCard
-                key={item._id}
-                item={item}
-                onCardClick={onCardClick}
-                onDelete={onDelete}
-                onCardLike={onCardLike}
-              />
-            );
-          })}{' '}
-        ? (
         <button
           onClick={handleAddClick}
           type="button"
           className="clothes-section__add-new"
         >
-          {' '}
           + Add New
         </button>
-        ) : null
       </div>
       {currentUser ? (
         <ul className="clothes-section__items">
-          {clothingItems.map((item) => {
-            return (
-              <ItemCard
-                key={item._id}
-                item={item}
-                onCardClick={onCardClick}
-                onDelete={onDelete}
-                onCardLike={onCardLike}
-              />
-            );
-          })}
+          {clothingItems
+            .filter((item) => item.owner === currentUser._id)
+            .map((item) => {
+              return (
+                <ItemCard
+                  key={item._id}
+                  item={item}
+                  onCardClick={onCardClick}
+                  onDelete={onDelete}
+                  onCardLike={onCardLike}
+                />
+              );
+            })}
         </ul>
       ) : (
         <p>Sign Up or Log In To View Your Items!</p>
